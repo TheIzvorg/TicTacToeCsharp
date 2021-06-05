3d2b443e50422015fe007355dffbb753e2a50d01
TheIzvorg <TheIzvorg@DESKTOP-4IKOL7T>
2021-06-05 11:47:29 +0300

    AI.cs: 1) Были добавлены интерфейсы: 1. INeuronSignal 2. INeuronNet 3. INeuronLayer 4. INeuron 5. INeuronReceptor 2) Были добавлены классы: 1. Neuron 2. NeuronFactor Field.cs: 1) Был добавлен метод вывода поля на консоль

--- TicTacToeAI/AI.cs
+++ TicTacToeAI/AI.cs
@@ -6,6 +6,122 @@ using System.Threading.Tasks;
 
 namespace TicTacToeAI
 {
+    #region Interfaces
+    public interface INeuronSignal
+    {
+        double Output { get; set; }
+    }
+    public interface INeuronNet
+    {
+        INeuronLayer InputLayer { get; set; }
+        INeuronLayer HiddenLayer { get; set; }
+        INeuronLayer OutPut { get; set; }
+
+        void Pulse(INeuronNet net);
+        void ApplyLearning(INeuronNet net);
+    }
+    public interface INeuronLayer : IList<INeuron>
+    {
+        void Pulse(INeuronNet net);
+        void ApplyLearning(INeuronNet net);
+    }
+    public interface INeuron : INeuronReceptor, INeuronSignal
+    {
+        void Pulse(INeuronLayer layer);
+        void ApplyLearning(INeuronLayer layer);
+        NeuronFactor Bias { get; set; }
+        double BiasWeight { get; set; }
+        double Error { get; set; }
+    }
+    public interface INeuronReceptor
+    {
+        Dictionary<INeuronSignal,NeuronFactor> Input { get; }
+    }
+    #endregion
+    #region Classes
+    public class Neuron : INeuron
+    {
+        private NeuronFactor m_bias;
+        private double m_biasWeight;
+        private double m_error;
+        private Dictionary<INeuronSignal, NeuronFactor> m_input;
+        private double m_output;
+
+        public NeuronFactor Bias
+        {
+            get { return m_bias; }
+            set { m_bias = value; }
+        }
+        public double BiasWeight
+        {
+            get { return m_biasWeight; }
+            set { m_biasWeight = value; }
+        }
+        public double Error
+        {
+            get { return m_error; }
+            set { m_error = value; }
+        }
+        public Dictionary<INeuronSignal, NeuronFactor> Input
+        {
+            get { return m_input; }
+        }
+        public double Output
+        {
+            get { return m_output; }
+            set { m_output = value; }
+        }
+        public void Pulse(INeuronLayer layer)
+        {
+            lock (layer)
+            {
+                m_output = 0;
+                foreach (KeyValuePair<INeuronSignal, NeuronFactor> item in m_input)
+                    m_output += item.Key.Output * item.Value.Weight;
+                m_output += m_bias.Weight * BiasWeight;
+                m_output = Sigmoid(m_output);
+            }
+        }
+        public void ApplyLearning(INeuronLayer layer)
+        {
+            lock (layer)
+            {
+                m_output += m_biasWeight;
+                m_biasWeight = 0;
+            }
+        }
+        private double Sigmoid(double value)
+        {
+            return value / (1 + Math.Abs(value));
+        }
+    }
+    public class NeuronFactor
+    {
+        private double m_weight;
+        private double m_delta;
+        public double Weight
+        {
+            get { return m_weight; }
+            set { m_weight = value; }
+        }
+        public double Delta
+        {
+            get { return m_delta; }
+            set { m_delta = value; }
+        }
+        public NeuronFactor(double Weight)
+        {
+            m_weight = Weight;
+            m_delta = 0;
+        }
+
+        public void ApplyDelta()
+        {
+            m_weight += m_delta;
+            m_delta = 0;
+        }
+    }
+    #endregion
     public class AI
     {
         protected int[,] Field { get; private set; }
@@ -21,7 +137,7 @@ namespace TicTacToeAI
 
         public override string ToString()
         {
-            return "Information:\n Version: v0.0.1";
+            return "Information:\n Version: v0.0.2";
         }
     }
 }
--- TicTacToeCsharpAI/Field.cs
+++ TicTacToeCsharpAI/Field.cs
@@ -6,8 +6,10 @@ using System.Threading.Tasks;
 
 namespace TicTacToeCsharp
 {
+    
     class Field
     {
+        
         const int dimensionSize = 3;
         private int[,] field = new int[dimensionSize, dimensionSize];
         public int[,] getField()
@@ -18,6 +20,51 @@ namespace TicTacToeCsharp
         {
             for(int i = 0; i < field.Length; i++)
                 field[i / dimensionSize, i % dimensionSize] = 0;
+        }
+        enum STATUS
+        {
+            ZERO = 0,
+            X = 1,
+            O = 2
+        }
+        public void ShowField()
+        {
+            /*for(int i = 0; i < field.Length; i++)
+            {
+                if (i % 3 == 0) 
+                    Console.WriteLine();
+                if (field[i/dimensionSize,i%dimensionSize] == (int)STATUS.ZERO)
+                    Console.Write("  ");
+                else if (field[i / dimensionSize, i % dimensionSize] == (int)STATUS.X)
+                    Console.Write(" X");
+                else if (field[i / dimensionSize, i % dimensionSize] == (int)STATUS.O)
+                    Console.Write(" O");
+            }*/
+            
+            for (int i = 0; i < dimensionSize; i++)
+            {
+                for (int j = 0; j < dimensionSize; j++)
+                {
+                    if (field[i, j] == 0)
+                    {
+                        Console.Write("  ");
+                    }
+                    else if (field[i, j] == 1)
+                    {
+                        Console.Write(" X");
+                    }
+                    else if (field[i, j] == 2)
+                    {
+                        Console.Write(" O");
+                    }
+                    else
+                    {
+                        throw new Exception("Не может быть такого значения");
+                    }
+                    //Console.Write(field[i,j]);
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
